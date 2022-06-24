c0d920d86f56471f12bd2e48753e183d11f5c250
Nataneal1 <106731957+Nataneal1@users.noreply.github.com>
2022-06-24 11:02:37 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-def recursiveMin(data):
-    if len(data) == 0:
-        return None
-    if len(data) == 1:
-        return data[0]
-    return min(data[0], recursiveMin(data[1:]))
+import sqlite3
+conn = sqlite3.connect('data.db',check_same_thread=False)
+c = conn.cursor()
 
 
-def recursiveMax(data):
-    if len(data) == 0:
-        return None
-    if len(data) == 1:
-        return data[0]
-    return max(data[0], recursiveMax(data[1:]))
+def create_table():
+	c.execute('CREATE TABLE IF NOT EXISTS taskstable(task TEXT,task_status TEXT,task_due_date DATE)')
 
 
-print("min", recursiveMin([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-print("mix", recursiveMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+def add_data(task,task_status,task_due_date):
+	c.execute('INSERT INTO taskstable(task,task_status,task_due_date) VALUES (?,?,?)',(task,task_status,task_due_date))
+	conn.commit()
+
+
+def view_all_data():
+	c.execute('SELECT * FROM taskstable')
+	data = c.fetchall()
+	return data
+
+def view_all_task_names():
+	c.execute('SELECT DISTINCT task FROM taskstable')
+	data = c.fetchall()
+	return data
+
+def get_task(task):
+	c.execute('SELECT * FROM taskstable WHERE task="{}"'.format(task))
+	data = c.fetchall()
+	return data
+
+def get_task_by_status(task_status):
+	c.execute('SELECT * FROM taskstable WHERE task_status="{}"'.format(task_status))
+	data = c.fetchall()
+
+
+def edit_task_data(new_task,new_task_status,new_task_date,task,task_status,task_due_date):
+	c.execute("UPDATE taskstable SET task =?,task_status=?,task_due_date=? WHERE task=? and task_status=? and task_due_date=? ",(new_task,new_task_status,new_task_date,task,task_status,task_due_date))
+	conn.commit()
+	data = c.fetchall()
+	return data
+
+def delete_data(task):
+	c.execute('DELETE FROM taskstable WHERE task="{}"'.format(task))
+	conn.commit()
